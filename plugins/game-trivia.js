3a4e5ed28f91b685c1ccaf8ee89169ed2a068ae3
RobleUY <151939411+Fede55xd@users.noreply.github.com>
2024-01-01 15:39:03 -0300

    act. juegos

---
+++ plugins/game-trivia.js
@@ -0,0 +1,43 @@
+// Créditos a Katashi Fukushima
+// Créditos a Wilson Waoz
+
+import fs from 'fs'
+
+let timeout = 10000
+let poin = 500
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    let id = m.chat
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'Todavía hay una pregunta sin responder en este chat', conn.tekateki[id][0])
+        throw false
+    }
+    let tekateki = JSON.parse(fs.readFileSync(`./src/game/trivia.js`))
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+    let _clue = json.response
+    let clue = _clue.replace(/[A-Za-z]/g, '_')
+    let caption = `
+ⷮ ${json.question}
+
+*• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
+*• Bono:* +${poin} Exp
+
+💫 Responde a este mensaje con la letra de la opción correcta ✅
+¡Tienes 10 segundos!
+`.trim()
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*La respuesta es la opción:* ${json.response}`, conn.tekateki[id][0])
+            delete conn.tekateki[id]
+        }, timeout)
+    ]
+}
+
+handler.help = ['trivia']
+handler.tags = ['game']
+handler.command = /^(trivia|triviador)$/i
+
+export default handler
