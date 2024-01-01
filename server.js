e398980211d4eaad7997012bc2ee67fd6303d2a1
RobleUY <151939411+Fede55xd@users.noreply.github.com>
2023-12-31 23:03:38 -0300

    Modificado server.js

--- server.js
+++ server.js
@@ -1,61 +1,148 @@
 import express from 'express' 
+
 import {createServer} from 'http'
+
 import path from 'path'
+
 import {Socket} from 'socket.io'
+
 import {toBuffer} from 'qrcode'
+
 import fetch from 'node-fetch'
 
+
+
 function connect(conn, PORT) {
+
   const app = global.app = express()
+
   console.log(app)
+
   const server = global.server = createServer(app)
+
   let _qr = 'QR invalido, probablemente ya hayas escaneado el QR.'
 
+
+
   conn.ev.on('connection.update', function appQR({qr}) {
+
     if (qr) _qr = qr
+
   })
+
+
 
   app.use(async (req, res) => {
+
     res.setHeader('content-type', 'image/png')
+
     res.end(await toBuffer(_qr))
+
   })
+
+
 
   server.listen(PORT, () => {
+
     console.log('App listened on port', PORT)
-    (opts['keepalive'] = true;
+
+import express from 'express' 
+
+import {createServer} from 'http'
+
+import path from 'path'
+
+import {Socket} from 'socket.io'
+
+import {toBuffer} from 'qrcode'
+
+import fetch from 'node-fetch'
+
+
+
+function connect(conn, PORT) {
+
+  const app = global.app = express()
+
+  console.log(app)
+
+  const server = global.server = createServer(app)
+
+  let _qr = 'QR invalido, probablemente ya hayas escaneado el QR.'
+
+
+
+  conn.ev.on('connection.update', function appQR({qr}) {
+
+    if (qr) _qr = qr
+
   })
+
+
+
+  app.use(async (req, res) => {
+
+    res.setHeader('content-type', 'image/png')
+
+    res.end(await toBuffer(_qr))
+
+  })
+
+
+
+  server.listen(PORT, () => {
+
+    console.log('App listened on port', PORT)
+
+    opts['keepalive'] = true;
+
+    if (opts['keepalive']) keepAlive()
+
+  })
+
 }
+
+
 
 function pipeEmit(event, event2, prefix = '') {
+
   const old = event.emit;
+
   event.emit = function(event, ...args) {
+
     old.emit(event, ...args)
+
     event2.emit(prefix + event, ...args)
+
   }
+
   return {
+
     unpipeEmit() {
+
       event.emit = old
+
     }}
+
 }
 
+
+
 function keepAlive() {
-  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
-  if (/(\/\/|\.)undefined\./.test(url)) return
+
 
 
   setInterval(async() => {
 
     console.log("=======================================");
 
-    //console.log(`keepAlive() url ->`, url);
-
     console.log(`keepAlive() a LocalHost 3000`);
 
     console.log("=======================================");
 
-    //fetch("http://localhost:3000").catch(console.error);
+    //const res = await fetch("http://localhost:3000");
 
-    const res = await fetch("http://localhost:3000");
+    const res = await fetch("https://stickers-602m.onrender.com"); //add url app de Roble
 
     if (res.status === 200) {
 
@@ -66,3 +153,74 @@ function keepAlive() {
     }
 
   }, 5 * 1000 * 60)
+
+  
+
+  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
+
+  if (/(\/\/|\.)undefined\./.test(url)) return
+
+  setInterval(() => {
+
+    fetch(url).catch(console.error)
+
+  }, 5 * 1000 * 60)
+
+}
+
+
+
+export default connect
+
+￼Enter    opts['keepalive'] = true;
+
+    if (opts['keepalive']) keepAlive()
+
+  })
+
+}
+
+
+
+function pipeEmit(event, event2, prefix = '') {
+
+  const old = event.emit;
+
+  event.emit = function(event, ...args) {
+
+    old.emit(event, ...args)
+
+    event2.emit(prefix + event, ...args)
+
+  }
+
+  return {
+
+    unpipeEmit() {
+
+event.emit = old
+
+    }}
+
+}
+
+
+
+function keepAlive() {
+
+
+
+  setInterval(async() => {
+
+    console.log("=======================================");
+
+    console.log(`keepAlive() a LocalHost 3000`);
+
+    console.log("=======================================");
+
+    //const res = await fetch("http://localhost:3000");
+
+    const res = await fetch("https://stickers-602m.onrender.com"); //add url app de Roble
+
+    if (res.status === 200) {
+
