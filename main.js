d9352bcafe57bd04aaf80b75f0158bda574878cf
RobleUY <151939411+Fede55xd@users.noreply.github.com>
2024-01-01 15:02:57 -0300

    cambiomain.js

--- main.js
+++ main.js
@@ -226,13 +226,12 @@ console.log(chalk.bold.white(chalk.bgMagenta(`CÓDIGO DE VINCULACIÓN:`)), chalk
 
 conn.isInit = false
 conn.well = false
-
 if (!opts['test']) {
 if (global.db) setInterval(async () => {
 if (global.db.data) await global.db.write()
 if (opts['autocleartmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp', "GataJadiBot"], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '2', '-type', 'f', '-delete'])))}, 30 * 1000)}
-if (opts['server']) (await import('./server.js')).default(global.conn, PORT)
 opts['server']=true;
+if (opts['server']) (await import('./server.js')).default(global.conn, PORT)
 
 async function connectionUpdate(update) {  
 const {connection, lastDisconnect, isNewLogin} = update
