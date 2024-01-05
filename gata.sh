4a9cae4621075e05195fe42e93703be823a06cf2
RobleUY <151939411+Fede55xd@users.noreply.github.com>
2024-01-05 11:46:38 -0300

    Update gata.sh

--- gata.sh
+++ gata.sh
@@ -206,7 +206,7 @@ git clone https://github.com/Fede55xd/stick.git
 echo -e "\033[01;32m\033[01mLa clonación se ha descargado e instalado correctamente.\nThe clone has been downloaded and installed successfully.\n\033[0m"
 
 echo -e "\033[01;32m\033[01mCambiando al directorio del repositorio!!\nChanging to the repository directory!!\n\033[0m" 
-cd stick
+cd RobleBOT
 
 echo -e "\e[36m
 ██╗░░░██╗██████╗░██████╗░░█████╗░████████╗███████╗  ██╗░░░██╗░█████╗░██████╗░███╗░░██╗
