74990936d83b69615d064a9a39303b3cbf158866
RobleUY <151939411+Fede55xd@users.noreply.github.com>
2024-02-12 20:24:51 -0300

    𝐑𝐨𝐛𝐥𝐞𝐔𝐘

---
+++ src/game/completarlap.js
@@ -0,0 +1,16 @@
+[
+{"question": "Completa la palabra: D_I__RO", "response": "DINERO" },
+{"question": "Completa la palabra: HOM_R_", "response": "HOMBRE" },
+{"question": "Completa la palabra: FA_S_", "response": "FALSO" },
+{"question": "Completa la palabra: CH__O", "response": "CHICO" },
+{"question": "Completa la palabra: A_TO", "response": "AUTO" },
+{"question": "Completa la palabra: M_T_", "response": "MOTO" },
+{"question": "Completa la palabra: V_D__O", "response": "VIDRIO" },
+{"question": "Completa la palabra: B_A_O", "response": "ARROZ" },
+{"question": "Completa la palabra: D_M__GO", "response": "DOMINGO" },
+{"question": "Completa la palabra: D_R_I_", "response": "DORMIR" }
+
+
+
+
+]
---
+++ src/game/encontrarlap.js
@@ -0,0 +1,33 @@
+[
+{"question": "Encuentra la palabra: IDCMROBLEBOTOA", "response": "ROBLEBOT" },
+{"question": "Encuentra la palabra: ULHUMANOCICHLO", "response": "HUMANO" },
+{"question": "Encuentra la palabra: HHNSLAVADORARR", "response": "LAVADORA" },
+{"question": "Encuentra la palabra: ININTELIGENTED", "response": "INTELIGENTE" },
+{"question": "Encuentra la palabra: PISHPIEDRAVSPD", "response": "PIEDRA" },
+{"question": "Encuentra la palabra: TALMADERAYAXZP", "response": "MADERA" },
+{"question": "Encuentra la palabra: ROLSHMUGREPOFB", "response": "MUGRE" },
+{"question": "Encuentra la palabra: ALCVINOHZXZPAM", "response": "VINO" },
+{"question": "Encuentra la palabra: MUSINUNCAXQPOJ", "response": "NUNCA" },
+{"question": "Encuentra la palabra: DISIEMPREDMBRE", "response": "SIEMPRE" },
+{"question": "Encuentra la palabra: VENEROZJSOBZDC", "response": "ENERO" },
+{"question": "Encuentra la palabra: SHOGMAGOUXZMXI", "response": "MAGO" },
+{"question": "Encuentra la palabra: LAESRAMPAXCYAS", "response": "RAMPA" },
+{"question": "Encuentra la palabra: LACROCDHROCAKD", "response": "ROCA" },
+{"question": "Encuentra la palabra: DULCNUEVOSZXBI", "response": "NUEVO" },
+{"question": "Encuentra la palabra: OOROHZDOLORVZM", "response": "DOLOR" },
+{"question": "Encuentra la palabra: NUCHSPREGUNTAG", "response": "PREGUNTA" },
+{"question": "Encuentra la palabra: SLJDJDSPALOFJO", "response": "PALO" },
+{"question": "Encuentra la palabra: FISÑJFSAFIDEOM", "response": "FIDEO" },
+{"question": "Encuentra la palabra: ZIEBFNUMUJERSO", "response": "MUJER" },
+{"question": "Encuentra la palabra: NONBRHOMBREKWÑ", "response": "HOMBRE" },
+{"question": "Encuentra la palabra: BXEBUAUTOBXBZP", "response": "AUTO" },
+{"question": "Encuentra la palabra: SKAPMFNCJUEGOD", "response": "JUEGO" },
+{"question": "Encuentra la palabra: CELULTELEFONOJ", "response": "TELEFONO" },
+{"question": "Encuentra la palabra: MSBXLSCABEZASP", "response": "CABEZA" },
+{"question": "Encuentra la palabra: APPWYDINEROZÑK", "response": "DINERO" }
+
+
+
+
+
+]
