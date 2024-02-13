f25e9cb5c61bbb066203c5786c27aed013cf544c
RobleUY <151939411+Fede55xd@users.noreply.github.com>
2024-02-12 21:28:54 -0300

    𝐑𝐨𝐛𝐥𝐞𝐔𝐘

--- plugins/RPG-CADA-DIA.js
+++ plugins/RPG-CADA-DIA.js
@@ -3,18 +3,34 @@ let handler = async (m, { conn }) => {
 
     let currentTime = new Date();
     let timeDiff = currentTime - user.lastCadadia;
-    let waitTime = 86400000; // 24 horas en milisegundos
+    let waitTime = 86400000; 
 
-    if (timeDiff < waitTime)
-        return m.reply(`*⏰ DEBES ESPERAR ${Math.ceil((waitTime - timeDiff) / 1000)} SEGUNDOS ANTES DE USAR "CADA DÍA" NUEVAMENTE.*\n*ℹ️ Veces utilizadas: ${user.cadadiaCount}*`, null, { contextInfo: null });
+    if (timeDiff < waitTime) {
+        let remainingTime = waitTime - timeDiff;
+        let hours = Math.floor(remainingTime / 3600000);
+        let minutes = Math.floor((remainingTime % 3600000) / 60000);
+        let seconds = Math.floor((remainingTime % 60000) / 1000);
 
-    let coinsClaimed = 100000; // Cambiado a 5,000,000 para representar una recompensa diaria más grande
+        let timeMessage = "";
+
+        if (hours > 0) {
+            timeMessage += `${hours} ${hours === 1 ? 'hora' : 'horas'}`;
+            if (minutes > 0) timeMessage += ` y ${minutes} ${minutes === 1 ? 'minuto' : 'minutos'}`;
+        } else if (minutes > 0) {
+            timeMessage += `${minutes} ${minutes === 1 ? 'minuto' : 'minutos'}`;
+            if (seconds > 0) timeMessage += ` y ${seconds} ${seconds === 1 ? 'segundo' : 'segundos'}`;
+        }
+
+        return m.reply(`*⏰ Debes esperar: ${timeMessage} para reclamar esta recompensa nuevamente.*`, null, { contextInfo: null });
+    }
+
+    let coinsClaimed = 100000; 
 
     user.money += coinsClaimed;
     user.lastCadadia = currentTime;
-    user.cadadiaCount = user.cadadiaCount ? user.cadadiaCount + 1 : 1; // Contador de veces utilizadas
+    user.cadadiaCount = user.cadadiaCount ? user.cadadiaCount + 1 : 1; 
 
-    let cadadiaMessage = `*💰 Has reclamado tu beneficio de 100000 RobleCoins al dia por ser parte de RobleBOT 💰*`;
+    let cadadiaMessage = `*💰 Has reclamado tu beneficio de 100000 RobleCoins al día por ser parte de RobleBOT 💰*`;
 
     return m.reply(cadadiaMessage, null, { contextInfo: null });
 }
