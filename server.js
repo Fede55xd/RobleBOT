cd3c96ce25bb83a62d28745bfe6ea382e6717ba3
RobleUY <151939411+Fede55xd@users.noreply.github.com>
2024-03-07 14:16:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,31 +7,22 @@ import fetch from 'node-fetch'
 
 function connect(conn, PORT) {
   const app = global.app = express()
-  //console.log(app)
+  console.log(app)
   const server = global.server = createServer(app)
   let _qr = 'QR invalido, probablemente ya hayas escaneado el QR.'
 
   conn.ev.on('connection.update', function appQR({qr}) {
-    if (qr) {
-      _qr = qr
-      if (global.keepAliveRender === 1 && process.env.RENDER_EXTERNAL_URL) {
-        console.log(`Para obtener el código QR ingresa a ${process.env.RENDER_EXTERNAL_URL}/get-qr-code`);
-      }
-    } 
+    if (qr) _qr = qr
   })
-  
-  app.get('/get-qr-code', async (req, res) => {
+
+  app.use(async (req, res) => {
     res.setHeader('content-type', 'image/png')
     res.end(await toBuffer(_qr))
-  });
+  })
 
-  app.get('*', async (req, res) => {
-    res.json("GATABOT-MD en ejecución");
-  });
-
-  server.listen(PORT, async () => {
+  server.listen(PORT, () => {
     console.log('App listened on port', PORT)
-    if (global.keepAliveRender === 1) await keepAliveHostRender();
+    opts['keepalive'] = true;
     if (opts['keepalive']) keepAlive()
   })
 }
@@ -49,32 +40,24 @@ function pipeEmit(event, event2, prefix = '') {
 }
 
 function keepAlive() {
+
+  setInterval(async() => {
+    console.log("=======================================");
+    console.log(`keepAlive() a LocalHost 3000`);
+    console.log("=======================================");
+    //const res = await fetch("http://localhost:3000");
+    const res = await fetch("https://roblebot.onrender.com"); //add url app de Roble
+    if (res.status === 200) {
+      const result = await res.text();
+      console.log(`Resultado desde LocalHost 3000`, result);
+    }
+  }, 5 * 1000 * 60)
+  
   const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
   if (/(\/\/|\.)undefined\./.test(url)) return
   setInterval(() => {
     fetch(url).catch(console.error)
   }, 5 * 1000 * 60)
-}
-
-
-//Kurt18: Esta función va impedir que Render vaya a modo suspensión por inactividad
-const keepAliveHostRender = async () => {
-  try {
-      setInterval(async() => {
-        if (process.env.RENDER_EXTERNAL_URL) {
-          const urlRender = process.env.RENDER_EXTERNAL_URL;
-          const res = await fetch(urlRender);
-          if (res.status === 200) {
-            const result = await res.text();
-            console.log(`Resultado desde keepAliveHostRender() ->`, result);
-          }
-        } else {
-          console.log(`No se encontró URL en Host Render.com. Por favor ir a config.js y modificar a cero el valor de: global.keepAliveRender`);
-        }
-      }, 5 * 1000 * 60)
-  } catch (error) {
-    console.log(`Error manejado en server.js keepAliveHostRender() detalles: ${error}`);
-  }
 }
 
 export default connect
